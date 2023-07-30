2e3fe288469eacdc57e2d51c9244484d45b7d2eb
LavanyaThangapandiyan2000 <lavanya.thangapandiyan@chainsys.com>
2023-07-30 13:25:12 +0530

    changes

--- src/main/java/com/project/fashion/validation/Validation.java
+++ src/main/java/com/project/fashion/validation/Validation.java
@@ -12,8 +12,8 @@ public class Validation {
 		boolean names = m.matches();
 		if (names)
 			return true;
-		else
-			return false;
+		return names;
+		
 	}
 
 	public String adminEmailValidation(String email) {
@@ -32,8 +32,8 @@ public class Validation {
 		boolean mail = m.matches();
 		if (mail)
 			return true;
-		else
-			return false;
+		return mail;
+		
 	}
 
 	public boolean passwordValidation(String password) {
@@ -42,18 +42,17 @@ public class Validation {
 		boolean pw = m.matches();
 		if (pw)
 			return true;
-		else
-			return false;
+		return pw;
 	}
 
 	public boolean phoneNumberValidation(String phoneNumber) {
-		Pattern p = Pattern.compile("(0|91)?[6-9][0-9]{9}");
+		Pattern p = Pattern.compile("(0|91)?[6-9]\\d{9}");
 		Matcher m = p.matcher(phoneNumber);
 		boolean phn = m.matches();
 		if (phn)
 			return true;
-		else
-			return false;
+		return phn;
+		
 	}
 
 }
